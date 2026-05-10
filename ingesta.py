273b46ec5bf5f155d7280e667ae495df0b458897
Darleneescobar <darlenepriyankaescobarhinojosa@gmail.com>
2026-05-10 10:28:45 -0500

    Configuracion MySQL y exportacion CSV

--- ingesta.py
+++ ingesta.py
@@ -1,10 +1,48 @@
+import mysql.connector
+import pandas as pd
 import boto3
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+
+conexion = mysql.connector.connect(
+    host="host.docker.internal",
+    port=3307,
+    user="root",
+    password="123456",
+    database="prueba_ingesta"
+)
+
+cursor = conexion.cursor()
+
+
+cursor.execute("SELECT * FROM alumnos")
+
+datos = cursor.fetchall()
+
+
+df = pd.DataFrame(
+    datos,
+    columns=["id", "nombre", "carrera"]
+)
+
+archivo = "alumnos.csv"
+
+df.to_csv(archivo, index=False)
+
+print("CSV generado correctamente")
+
+
+nombreBucket = "ingesta-02"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
-print(response)
 
-print("Ingesta completada")
+s3.upload_file(
+    archivo,
+    nombreBucket,
+    archivo
+)
+
+print("Archivo subido a S3")
+
+
+cursor.close()
+conexion.close()
